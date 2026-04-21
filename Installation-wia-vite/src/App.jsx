b9492f5313b296f3080f00d4b7f68f21d90d1eec
rohit-specs <rohit5592006@gmail.com>
2026-04-21 16:16:23 +0530

    Updated

--- Installation-wia-vite/src/App.jsx
+++ Installation-wia-vite/src/App.jsx
@@ -1,7 +1,9 @@
 import { Fragment, useReducer, useState } from "react"
 
 function App(){
-    const [state,setState] = useState(0)
+    const initialCount = 0
+    const finalCount = 10
+    const [state,setState] = useState(initialCount)
     function reducer(count,action){
         switch (action.type) {
             case "INCREASE+1":
@@ -17,32 +19,34 @@ function App(){
                 count = count-2
                 break;
             case "RESET":
-                return 0
+                return initialCount
             default:
                 return count
         }
-        if (count>10) {
-            return 10
+        if (count>finalCount) {
+            return finalCount
         }
-        if (count<0){
-            return 0
+        if (count<initialCount){
+            return initialCount
         }
         return count
 
     }
-    const [count,dispatch] = useReducer(reducer,0)
+    const [count,dispatch] = useReducer(reducer,initialCount)
 
 
     return(
         <div className="text-center">
+        <div className="fs-1 mb-2">Counter via useState</div>
         <h1 className=" display-1 fw-bold">{state}</h1>
         <div className="d-flex justify-content-center gap-2">
-        <button className="fs-4 btn btn-success" onClick={()=>setState( prev=> Math.min(10,prev+1) )}>Increment +1</button>
-        <button className="fs-4 btn btn-primary" onClick={()=>setState(prev=> Math.max(0,prev-1))}>Decrease -1</button>
-        <button className="fs-4 btn btn-success" onClick={()=>setState(prev=> Math.min(10,prev+2))}>Increment +2</button>
-        <button className="fs-4 btn btn-primary" onClick={()=>setState(prev=> Math.max(0,prev-2))}>Decrease -2</button>
-        <button className="fs-4 btn btn-danger" onClick={()=>setState(0)}>RESET</button>
+        <button className="fs-4 btn btn-success" onClick={()=>setState( prev=> Math.min(finalCount,prev+1) )}>Increment +1</button>
+        <button className="fs-4 btn btn-primary" onClick={()=>setState(prev=> Math.max(initialCount,prev-1))}>Decrease -1</button>
+        <button className="fs-4 btn btn-success" onClick={()=>setState(prev=> Math.min(finalCount,prev+2))}>Increment +2</button>
+        <button className="fs-4 btn btn-primary" onClick={()=>setState(prev=> Math.max(initialCount,prev-2))}>Decrease -2</button>
+        <button className="fs-4 btn btn-danger" onClick={()=>setState(initialCount)}>RESET</button>
         </div>
+        <div className="fs-1 mb-2">Counter via userReducer</div>
         <h2 className=" display-2 fw-bold">{count}</h2>
         <div className="d-flex justify-content-center gap-2">
         <button className="fs-4 btn btn-success" onClick={()=> dispatch({type:"INCREASE+1"})}>Increment +1</button>
