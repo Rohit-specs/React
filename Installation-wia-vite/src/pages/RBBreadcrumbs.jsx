f46d5fcabc68a348207af7486ecf8dd46244c4c8
rohit-specs <rohit5592006@gmail.com>
2026-05-11 16:32:56 +0530

    assignment updated

--- Installation-wia-vite/src/pages/RBBreadcrumbs.jsx
+++ Installation-wia-vite/src/pages/RBBreadcrumbs.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Breadcrumb ,} from 'react-bootstrap'
-import {Link} from 'react-router-dom'
-  
+import React, { Fragment } from 'react'
+import { Breadcrumb, } from 'react-bootstrap'
+import { CaretRight } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom'
+
 const CustomLink = (props) => {
-        const { href, children } = props;
-    return <Link to={href}>{children}</Link>
-    };
-    
+    const { href, children } = props;
+    return (
+        <Fragment>
+            <Link to={href}>{children}</Link>
+            <CaretRight className='mx-1'/>
+        </Fragment >
+    )
+};
+
 const RBBreadcrumbs = () => {
-     
+
     return (<Breadcrumb
         className="custom-breadcrumb"
     >
         <Breadcrumb.Item
             linkAs={CustomLink}
             href="/cloud"
+            bsPrefix=' '
         >
             Cloud
         </Breadcrumb.Item>
@@ -22,6 +29,7 @@ const RBBreadcrumbs = () => {
         <Breadcrumb.Item
             linkAs={CustomLink}
             href="/files"
+            bsPrefix=' '
         >
             Files
         </Breadcrumb.Item>
@@ -29,6 +37,7 @@ const RBBreadcrumbs = () => {
         <Breadcrumb.Item
             linkAs={CustomLink}
             href="/project"
+            bsPrefix=' '
         >
             Project
         </Breadcrumb.Item>
--- Installation-wia-vite/src/pages/RBCarousel.jsx
+++ Installation-wia-vite/src/pages/RBCarousel.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Image from 'react-bootstrap/Image';
+import Carousel from 'react-bootstrap/Carousel';
+import carouselData from './../data/RBCarouselData'
+import { ArrowLeftCircle, ArrowRightCircle, Circle} from 'react-bootstrap-icons';
+import { Button, Stack } from 'react-bootstrap';
 const RBCarousel = () => {
-  return (
-    <div>
-      
-    </div>
+  const [index, setIndex] = useState(0);
+  function handleSelect(newIndex) {
+    setIndex(selectedIndex);
+  }
+  return (<>
+    <Carousel activeIndex={index} onSelect={handleSelect} indicators={false}
+      prevIcon={
+        <ArrowLeftCircle size={30} className='opacity-75' />}
+      nextIcon={
+        <ArrowRightCircle size={30} className='opacity-75' />
+      }>
+      {carouselData.map((sliderData, index) => {
+        return (
+          <Carousel.Item key={index}>
+            <Image src={sliderData.image} className='rounded-4 w-100'></Image>
+            <Carousel.Caption>
+              <h3>{sliderData.title}</h3>
+              <p>{sliderData.content}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )
+      })}
+    </Carousel>
+    <div className='d-flex justify-content-center gap-2 my-3'>{carouselData.map((item,index) => {
+      return (
+
+        <Circle onClick={()=>setIndex(index)} role='button'></Circle>
+        
+      )
+    })}</div>
+    
+
+    <Button onClick={() => setIndex(2)}>Next</Button>
+    <Button onClick={() => setIndex(1)}>Prev</Button>
+    <div>Active Carousel item index: {index}</div></>
   )
 }
 
